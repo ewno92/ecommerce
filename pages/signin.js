56ae4ce2cd5f2ca9fc476d1ecd47ecc185ef0917
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-10-07 13:19:51 -0700

    login api created

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,57 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useState, useContext, useEffect } from "react";
+import { DataContext } from "../store/GlobalState";
+import { postData } from "../utils/fetchData";
+import Cookie from "js-cookie";
+import { useRouter } from "next/router";
 import { Container, Form, Button } from "react-bootstrap";
 const Signin = () => {
+  const initialState = { email: "", password: "" };
+  const [userData, setUserData] = useState(initialState);
+  const { email, password } = userData;
+
+  const { state, dispatch } = useContext(DataContext);
+  const { auth } = state;
+
+  const router = useRouter();
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+    dispatch({ type: "NOTIFY", payload: {} });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+    const res = await postData("auth/login", userData);
+
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+
+    dispatch({
+      type: "AUTH",
+      payload: {
+        token: res.access_token,
+        user: res.user,
+      },
+    });
+
+    Cookie.set("refreshtoken", res.refresh_token, {
+      path: "api/auth/accessToken",
+      expires: 7,
+    });
+
+    localStorage.setItem("firstLogin", true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(auth).length !== 0) router.push("/");
+  }, [auth]);
+
   return (
     <Container>
       <Head>
